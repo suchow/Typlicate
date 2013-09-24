2f8a0af1c416974b67aaf68d6ddc8d220fd7ef4e
Jordan Suchow <suchow@post.harvard.edu>
2013-09-24 02:35:31 -0400

    Register keystrokes and check accuracy w/r/t the text

--- Typlicate.js
+++ Typlicate.js
@@ -1,3 +1,33 @@
+Array.prototype.contains = function(obj) {
+  var i = this.length;
+  while (i--) {
+    if (this[i] === obj) {
+        return true;
+    }
+  }
+  return false;
+}
+
+// Should the two characters, c1 and c2, be treated as equivalent?
+var equivalent = function(c1, c2) {
+  // If there's a perfect match, accept.
+  if (c1 == c2) {
+    return true;
+  }
+
+  // If there's a near match, accept.
+  var d = { "\"": ["\“", "\”"],
+            "\’": ["\'"],
+            "—" : ["-"] ,
+          };
+
+  if (typeof d[c1] !== "undefined" && d[c1].contains(c2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 if (Meteor.isClient) {
 
   Meteor.startup(function () {
@@ -7,6 +37,8 @@ if (Meteor.isClient) {
   });
 
   Session.set("book", "Loading...");
+  numCompleted = 3;
+  positionInParagraph = 0;
 
   Meteor.startup(function () {
     Meteor.call('getBook', 'gatsby',
@@ -35,12 +67,37 @@ if (Meteor.isClient) {
     x = $("p");
     for (var i = 0; i <= x.length; i++) {
       x[i].id = i;
+      if (i >= numCompleted) {
+        x[i].className += "incomplete";
+      } else if (i == numCompleted+1) {
+        x[i].className += "current";
+      } else {
+        x[i].className += "complete";
+      }
     };
   };
 
   Template.text.events = {
     "keypress #inputter": function(event) {
-      console.log(String.fromCharCode(event.which));
+      event.preventDefault();
+      thisParagraph = $("#" + numCompleted).text();
+      currentSymbol = thisParagraph[positionInParagraph];
+      chosenSymbol = String.fromCharCode(event.which);
+
+      // If the current symbol is a newline character, give a free pass.
+      if (currentSymbol === "\n") {
+        positionInParagraph += 1;
+        currentSymbol = thisParagraph[positionInParagraph];
+      };
+
+      if (equivalent(chosenSymbol, currentSymbol)) {
+        positionInParagraph += 1;
+      }
+
+      if (positionInParagraph === thisParagraph.length) {
+        numCompleted += 1;
+        positionInParagraph = 0;
+      }
     }
   }
 }
